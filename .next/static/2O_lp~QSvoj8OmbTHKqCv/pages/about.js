affbf0ce41850743c4d632b88cf0e307e6387747
kondratyev <kondratyev.work@gmail.com>
2018-10-03 09:03:21 +0300

    init

---
+++ .next/static/2O_lp~QSvoj8OmbTHKqCv/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{207:function(e,n,t){__NEXT_REGISTER_PAGE("/about",function(){return e.exports=t(208),{page:e.exports.default}})},208:function(e,n,t){"use strict";t.r(n);var a=t(0),u=t.n(a),o=t(31);n.default=function(){return u.a.createElement(o.a,null,u.a.createElement("p",null,"This is the about page"))}}},[[207,1,0]]]);
